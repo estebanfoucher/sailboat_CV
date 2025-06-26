df86a888328d722c0345e9ec5550e3eb97e3fb3f
estebanfoucher <estefoucher@gmail.com>
2025-06-26 11:07:47 +0200

    feat:added export to tensoRT

---
+++ utils/model/export.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("runs/detect/train-03/weights/best.pt")  # load a custom trained model
+
+# Export the model
+model.export(format="engine", int8=True)
